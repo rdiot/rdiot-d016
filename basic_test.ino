83eafd776184daeb45f97b777db75dd312697745
rdiot <rd1004g@gmail.com>
2017-09-14 19:54:26 +0900

    Create basic_test.ino

---
+++ basic_test.ino
@@ -0,0 +1,19 @@
+/* LCD1602 I2C (LCD1602) [D016] : http://rdiot.tistory.com/227 [RDIoT Demo] */
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+ 
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x20 for a 16 chars and 2 line display
+ 
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+ 
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.print("Hello, world!");
+  
+  lcd.setCursor(0, 1);
+ 
+  lcd.print("cursor 0,1");
+}
